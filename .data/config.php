c206681f08a01a67861e3bac94020da2b5f8a5be
kylehao <kylehao@gmail.com>
2023-02-07 04:55:07 +0000

    up

---
+++ .data/config.php
@@ -0,0 +1,14 @@
+<?php $configs = '
+{
+    "admin": "jgwy1234",
+    "timezone": "8",
+    "disktag": "Sharelinkmusic",
+    "Sharelinkmusic": {
+        "Driver": "Sharelink",
+        "diskname": "U2hhcmVsaW5rbXVzaWM",
+        "domainforproxy": "a3VzdC42NjAwMDAueHl6",
+        "shareapiurl": "aHR0cHM6Ly9zdHVrdXN0ZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vX2FwaS92Mi4wL2RyaXZlcy9iIUdub09KMlpZdEV5VHRlSG84Y0ZKdm5EY1hnQ19ydTVKbDBiVHJfM25zSWFsTVJwTl80VG9SWkNSbUZEUkgzQzUvcm9vdA",
+        "shareurl": "aHR0cHM6Ly9zdHVrdXN0ZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vOmY6L2cvcGVyc29uYWwvc2hhcmVfc3R1X2t1c3RfZWR1X2NuL0VzNWJQb19zM0wxSnBxWnNzblRoNklNQlF3TUFrX3AzLVdzOTBLSU5ZWEZQRmc_ZT1CclFlNHc"
+    }
+}
+';
