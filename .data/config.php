d4c8a29be6a4980fb868369f3078df8a09c6675b
kylehao <kylehao@gmail.com>
2023-12-23 01:59:16 +0000

    Update config.php

--- .data/config.php
+++ .data/config.php
@@ -1,14 +1,40 @@
 <?php $configs = '
 {
-    "admin": "888775811",
+    "admin": "jgwy1234",
+    "passfile": "aXVvVUhTREFVUSBXRVMgdXUgM3VkNzgyLnR4dA",
     "timezone": "8",
-    "disktag": "Sharelinkmusic",
-    "Sharelinkmusic": {
+    "disktag": "musicstukust|Sharelink|free163person|ampsnju",
+    "musicstukust": {
         "Driver": "Sharelink",
-        "diskname": "U2hhcmVsaW5rbXVzaWM",
-        "domainforproxy": "a3VzdC42NjAwMDAueHl6",
-        "shareapiurl": "aHR0cHM6Ly9zdHVrdXN0ZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vX2FwaS92Mi4wL2RyaXZlcy9iIUdub09KMlpZdEV5VHRlSG84Y0ZKdm5EY1hnQ19ydTVKbDBiVHJfM25zSWFsTVJwTl80VG9SWkNSbUZEUkgzQzUvcm9vdA",
-        "shareurl": "aHR0cHM6Ly9zdHVrdXN0ZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vOmY6L2cvcGVyc29uYWwvc2hhcmVfc3R1X2t1c3RfZWR1X2NuL0VzNWJQb19zM0wxSnBxWnNzblRoNklNQlF3TUFrX3AzLVdzOTBLSU5ZWEZQRmc_ZT1CclFlNHc"
+        "diskname": "bXVzaWNzdHVrdXN0",
+        "domainforproxy": "YWFzZXUuODM4MzgzLnh5eg",
+        "public_path": "c2hhcmU",
+        "shareapiurl": "aHR0cHM6Ly9hYXNldWVkdS1teS5zaGFyZXBvaW50LmNvbS9fYXBpL3YyLjAvZHJpdmVzL2IhXzJkU2hPODBmVU9lTnJfNldzdm9DNE9ZMldJRHVSTkJzSEZpQmZtRlBFQk1XaEg4cnRTa1NwNGxOWkNJRXJEUC9yb290",
+        "shareurl": "aHR0cHM6Ly9hYXNldWVkdS1teS5zaGFyZXBvaW50LmNvbS86ZjovZy9wZXJzb25hbC8yMTMxODM5NjBfYWFfc2V1X2VkdV9jbi9FcEdSUXFOSUlobEN2ekNNZzR5dGIxZ0JVU1diQTd6SEZqNXh2ZkxlbGZTaFNBP2U9ZmpQaXFm"
+    },
+    "Sharelink": {
+        "Driver": "Sharelink",
+        "diskname": "U2hhcmVsaW5r",
+        "shareapiurl": "aHR0cHM6Ly9hYXNldWVkdS1teS5zaGFyZXBvaW50LmNvbS9fYXBpL3YyLjAvZHJpdmVzL2IhXzJkU2hPODBmVU9lTnJfNldzdm9DNE9ZMldJRHVSTkJzSEZpQmZtRlBFQk1XaEg4cnRTa1NwNGxOWkNJRXJEUC9yb290",
+        "shareurl": "aHR0cHM6Ly9hYXNldWVkdS1teS5zaGFyZXBvaW50LmNvbS86ZjovZy9wZXJzb25hbC8yMTMxODM5NjBfYWFfc2V1X2VkdV9jbi9FcEdSUXFOSUlobEN2ekNNZzR5dGIxZ0JVU1diQTd6SEZqNXh2ZkxlbGZTaFNBP2U9ZmpQaXFm"
+    },
+    "free163person": {
+        "Driver": "Onedrive",
+        "diskname": "ZnJlZTE2M3BlcnNvbg",
+        "public_path": "c2hhcmU",
+        "refresh_token": "M.C107_BAY.-CpubSZ3OwavuWWJWtd8kEbzy3Fd5L7NTsyo4evqm1DQChe3v3k3zvPqqtKavg*qY*WYMDMwdyGgMysxpvOinFM4fgbXXinncjwqPtN4fMc766SUyJghPy0I!99wo*iRk6eINUwAKFhbNqhpX7LT0LDUoLSUHpa4SeKaUWiKAvz8*4cJcX7mOUpOD4grEFmzI9FyWMH11Rpu7h83V8rHLGOrenKUQ*PylBJLZpnsBWv1GjGnRE!tjM!UHHmuj1ikfjHJD0u24KntWfZ2z1egrMXTauo9JqoWN0gVWMsUNwjKgFmOQKzmOAuxU9rdQcSN0Bef7G8dDbfBdaaGvThZcaVjAAHfvl1BbKMpH5wLCzxOf",
+        "token_expires": 1703813458
+    },
+    "ampsnju": {
+        "Driver": "Sharepoint",
+        "client_id": "458e8631-8b54-448d-b47c-452b2b01b206",
+        "client_secret": "MkZsNThCeXM1RWIzXzZ-QmNCOEMybUp4OTNiRS11fng1NQ",
+        "diskname": "YW1wc25qdQ",
+        "public_path": "c2hhcmU",
+        "refresh_token": "0.AXAA8hRLOg9cGU60Lp5QuDyi5DGGjkVUi41EtHxFKysBsgbEAII.AgABAAEAAAAmoFfGtYxvRrNriQdPKIZ-AgDs_wUA9P9ZIqVh-9OE4bIFoHINwNmkxwNDsA9mmwcmeUDkPdIDlM0kCJwpr2wai0nfeSmXoBYCKAlOl841ameZtdiAskMRh2HdjRkc-jToLqrRfXN2S2pmVbj0zAFxXUPFhqaWdYeFjUsvxrsZkG-05J_nFFh2-xsm7SLBIFtVydosCndO8iF-MGeLwSSiZSzVNpkIMAZ3qY9PnaGDN1sMqH_2rR0qhuTFGfa_HMha8CJmgCnzODQ1Bz3nZ5zsoKoycDacawETDjclADHuQS06gcfu8JpCtXrlocZSBvZuxczroz3JtcNXtNeiFQsmbTFnMBZq726WTZ2vHTBPRRzjPDU9j_TuN7qOcMrgkT7hYpzOEXT0RQWuX5MTSEr8tH33wUODOsvo8ozEHCs5JRU53RvN-rdK0Tcjuo-ItOZaq4D-zsSwmQW1slLUZzcpRwDZY9hFJGTFWaPBBrrIjnxubIWhAe6s-SnENPVEpckMecE3DhdMyvd3wBmeYPsl06AWn84mFaSXN8N8VHc6YurAuBdMS_N33KiDbMv1KbTtyQhbHs0S3U4f6oCQodbmvE1wal7dHz7T5Bl-Jac9pUSGX50x-8hDFAzAoXIKLTnT34h1iCU8fdfXKtgdBRxy__nfJ0s8KqGnlYIqGl8jGGYF8VslQT3r41pPPzc07cOxZMXKZwp7LPtutHAd8eLdvgREvA",
+        "sharepointSite": "aHR0cHM6Ly9hbXBzbmp1LnNoYXJlcG9pbnQuY29tL3NpdGVzL3NoYXJlbGlzdA",
+        "siteid": "ampsnju.sharepoint.com,407e4e8a-4be4-4426-b97c-4a23898b68ab,dd9da54c-afa6-402d-be86-25087d83c973",
+        "token_expires": 1703807028
     }
 }
 ';
